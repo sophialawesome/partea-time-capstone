bc7b6181596de6b9165063e5d748999d34eb3dc2
Sophia Law <sophialaw927@gmail.com>
2020-06-15 22:58:35 -0500

    worked on logout - seems to work fine. need to redirect new user to register page

--- src/components/ParteaTime.js
+++ src/components/ParteaTime.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import NavBar from "./nav/NavBar";
 import ApplicationViews from "./ApplicationViews";
 import "./ParteaTime.css";
 
 const ParteaTime = () => {
+    const isAuthenticated = () => sessionStorage.getItem("credentials") !== null;
+
+  const [hasUser, setHasUser] = useState(isAuthenticated());
+
+  const setUser = user => {
+    sessionStorage.setItem("credentials", JSON.stringify(user));
+    setHasUser(isAuthenticated());
+  };
+
+  const clearUser = () => {
+    sessionStorage.clear();
+    setHasUser(isAuthenticated());
+  }
+  
   return (
     <>
-       <NavBar />
-      <ApplicationViews /> 
+      <NavBar hasUser={hasUser} clearUser={clearUser}/>
+      <ApplicationViews hasUser={hasUser} setUser={setUser} />
     </>
   );
 };
+
 
 export default ParteaTime;
